480caf4694ba25d99b924f15a7ab2e2fdce19c48
Keshari Nandan <keshari.nandan2@gmail.com>
2021-12-22 20:49:10 +0530

    Learned about string litrals and raw string litrals in go

---
+++ string-litrals/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+
+	s = "How are you?"
+
+	s = `How are you?`
+
+	fmt.Println(s)
+
+	s = "<html>\n\t<body>\"Hello\"</body>\n</html>"
+	fmt.Println(s)
+
+	s = `
+<html>
+	<body>"Hello"</body>
+</html>`
+
+	fmt.Println(s)
+
+	// exceptions
+	fmt.Println("c:\\my\\dir\\file")
+	fmt.Println(`c:\my\dir\file`)
+}
